43e7423f9a5d45c7c4c62746ecb22d19d512aeed
datavistics <datavistics@gmail.com>
2017-11-08 15:45:32 +0400

    Added senior plots as well!

--- Community Impact and Opportunity/src/chloropleth.R
+++ Community Impact and Opportunity/src/chloropleth.R
@@ -7,8 +7,8 @@ names(community_score) <- c('region', 'value')
 community_score$region <- as.character(community_score$region)
 zip_choropleth(community_score,
                state_zoom = "pennsylvania",
-               title = "Pennsylvania Community score per zip code",
-               legend = "Community score")
+               title = "Pennsylvania Community Impact score per zip code",
+               legend = "Community Impact score")
 
 opportunity_score <- df[c(7,6)]
 names(opportunity_score) <- c('region', 'value')
@@ -26,3 +26,33 @@ zip_choropleth(composite_score,
                state_zoom = "pennsylvania",
                title = "Pennsylvania Composite score per zip code",
                legend = "Composite score")
+
+senior_pop <- read.csv("..\\data\\other_data.csv")
+senior_pop$Senior.Occupancy..SO. <- as.numeric(sub("%","",senior_pop$Senior.Occupancy..SO.))/100
+senior_pop$Senior.Rent.Burden..SRB. <- as.numeric(sub("%","",senior_pop$Senior.Rent.Burden..SRB.))/100
+senior_pop$Senior.Housing.Stock..SHS. <- as.numeric(sub("%","",senior_pop$Senior.Housing.Stock..SHS.))/100
+senior_pop$Zip.Code <- as.character(senior_pop$Zip.Code)
+
+occupancy <- senior_pop[c(1,2)]
+names(occupancy) <- c('region', 'value')
+occupancy$region <- as.character(occupancy$region)
+zip_choropleth(occupancy,
+               state_zoom = "pennsylvania",
+               title = "Pennsylvania Senior Occupancy per zip code",
+               legend = "Occupancy Percentage")
+
+rent_burden <- senior_pop[c(1,2)]
+names(rent_burden) <- c('region', 'value')
+rent_burden$region <- as.character(rent_burden$region)
+zip_choropleth(rent_burden,
+               state_zoom = "pennsylvania",
+               title = "Pennsylvania Senior Rent Burden per zip code",
+               legend = "Rent Burden Percentage")
+
+housing_stock <- senior_pop[c(1,2)]
+names(housing_stock) <- c('region', 'value')
+housing_stock$region <- as.character(housing_stock$region)
+zip_choropleth(housing_stock,
+               state_zoom = "pennsylvania",
+               title = "Pennsylvania Senior Housing Stock per zip code",
+               legend = "Housing Stock Percentage")
